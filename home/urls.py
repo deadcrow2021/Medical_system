002b1bca2787336ed1a34562108b38738d6152e8
Paul <z9099630061@yandex.ru>
2022-08-26 17:01:45 +0300

    Added main page, login page and styles for them

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('login.html', views.login_page, name='login')
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, 'home/index.html')
+    # user = "admin"
+    user = "doctor"
+    # user = "pat"
+    return render(request, 'home/index.html', { "user": user })
+
+def login_page(request):
+    user="anonimus"
+    return render(request, 'home/login.html', { "user": user })
