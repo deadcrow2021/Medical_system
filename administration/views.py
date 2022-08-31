8764234b13ffe9bfc22203aa2a4e1a19468addbc
Paul <z9099630061@yandex.ru>
2022-08-31 21:08:27 +0300

    Added pagination and message box

--- administration/views.py
+++ administration/views.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
@@ -6,16 +6,21 @@ from home.models import CustomUser
 from .models import Files
 from .forms import FileUploadForm
 from django.views.generic import CreateView, ListView
+from django.contrib import messages
 
 
 class AdminPageView(ListView):
+    paginate_by: int = 6
     model = CustomUser
     template_name: str = 'administration/admin_page.html'
+    context_object_name: Optional[str] = 'users'
     
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context |= { 'users': CustomUser.objects.exclude(groups='a') }
         return context
+    
+    def get_queryset(self):
+        return CustomUser.objects.exclude(groups='a')
 
 
 def files_page(request):
@@ -32,6 +37,7 @@ class UploadFilesView(CreateView):
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save(commit=True)
+            messages.success(request, message='Файл успешно добавлен')
             return redirect(self.success_url)
         else:
             return render(request, self.template_name, { 'form': form })
--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ def generate_username(first_name, date):
 
 def profile(request, profile_id):
     user_profile = CustomUser.objects.get(pk=profile_id)
-    return render(request, 'users/profile.html', {'profile':user_profile})
+    return render(request, 'users/profile.html', { 'profile': user_profile })
 
 
 class PatientsView(ListView):
