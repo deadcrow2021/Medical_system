050565372828335f1a20b12cb6e3ef92fa3aafe1
paul <z9099630061@yandex.ru>
2022-10-28 13:26:45 +0300

    Added patinet info to doctor home page

--- home/models.py
+++ home/models.py
@@ -23,7 +23,7 @@ class Patient(models.Model):
         return self.get_full_name()
     
     def get_full_name(self) -> str:
-        return f"{self.first_name} {self.last_name} {self.father_name}"
+        return f"{self.last_name} {self.first_name} {self.father_name}"
 
 
 ##### Medical Forms #####
